0fcf0633720051754d1a8037e4e2ec952cdb2e67
Yan Nascimento <yanzebedeu@hotmail.com>
2023-09-19 12:43:14 +0200

    utilizzo numeri random

--- classe_istanza/Program.cs
+++ classe_istanza/Program.cs
@@ -26,11 +26,12 @@ namespace classe_istanza
         }
         static void Main(string[] args)
         {
+            Random random= new Random();
             //istanzia gli oggetti
-            Prodotto p1 = new Prodotto("p1",20);
-            Prodotto p2 = new Prodotto("p2", 30);
-            
-
+            Prodotto p1 = new Prodotto("p1",random.Next(1,99));
+            Prodotto p2 = new Prodotto("p2", random.Next(1, 99));
+            Console.WriteLine("p1:\nNome: " + p1.getnome() + "\nPrezzo: " + p1.getprezzo());
+            Console.WriteLine("p2:\nNome: " + p2.getnome() + "\nPrezzo: " + p2.getprezzo());
         }
     }
 }
