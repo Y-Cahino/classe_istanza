43ea452ade09028d6fd3e20ae15bad3d730151f5
Yan Nascimento <yanzebedeu@hotmail.com>
2023-09-19 12:35:15 +0200

    istanzia oggetti

--- classe_istanza/Program.cs
+++ classe_istanza/Program.cs
@@ -1,20 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace classe_istanza
 {
-    class Prodotto
+    public class Prodotto
     {
-        public string nome;
-        public double prezzo;
-    }
-    internal class Program
-    {
+        private string nome;
+        private double prezzo;
+        public Prodotto(string nome, double prezzo)
+        {
+            this.nome = nome;
+            this.prezzo = prezzo;
+        }
+        public string getnome()
+        {
+            return nome;
+        }
+        public double getprezzo()
+        {
+            return prezzo;
+        }
         static void Main(string[] args)
         {
+            //istanzia gli oggetti
+            Prodotto p1 = new Prodotto("p1",20);
+            Prodotto p2 = new Prodotto("p2", 30);
+            
+
         }
     }
 }
